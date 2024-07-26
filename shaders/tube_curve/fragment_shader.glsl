54c4184f073c52d6b8dd5da3bb2046c35b6d2cd3
Tugdual <tugdualgaillard@gmail.com>
2024-07-26 15:53:29 +0200

    remove specular ligth causing artefact for small tubes

--- shaders/tube_curve/fragment_shader.glsl
+++ shaders/tube_curve/fragment_shader.glsl
@@ -6,7 +6,7 @@ in vec3 color;
 // Output color
 out vec4 FragColor;
 float ambient_light = 0.8;
-float specular_light_strength = 0.4;
+//float specular_light_strength = 0.4;
 vec3 light_position = vec3(2,2,2);
 void main()
 {
@@ -14,12 +14,12 @@ void main()
     vec3 light_direction = normalize(light_position);
     float diffusion = dot(light_direction, normal);
 
-    vec3 fragment_view_direction = normalize(vec3(0,0,1)-position);
-    vec3 reflected_light_direction = 2*dot(normal, light_direction) * normal-light_direction;
-    float specular_ligth = pow(max(dot(reflected_light_direction, fragment_view_direction), 0.0), 8);
-    specular_ligth *= specular_light_strength;
+    // vec3 fragment_view_direction = normalize(vec3(0,0,1)-position);
+    // vec3 reflected_light_direction = 2*dot(normal, light_direction) * normal-light_direction;
+    // float specular_ligth = pow(max(dot(reflected_light_direction, fragment_view_direction), 0.0), 8);
+    // specular_ligth *= specular_light_strength;
 
-    float intensity =  (specular_ligth + ambient_light + diffusion)/(1.0 + ambient_light);
+    float intensity =  (ambient_light + diffusion)/(1.0 + ambient_light);
 
     FragColor = vec4(color * intensity,1.0);
 }
